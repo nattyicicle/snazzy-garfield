dae3d50e7d199266332840cc04be95024dfb4b88
nattyicicle <nfkoziol@gmail.com>
2026-06-12 18:44:21 -0400

    NEXT PASS

--- lib/audio.ts
+++ lib/audio.ts
@@ -1,7 +1,8 @@
 export function resolveAudioSource(file: string) {
   const baseUrl = process.env.NEXT_PUBLIC_AUDIO_BASE_URL?.replace(/\/+$/, "");
+  const isBundledSetlistReference = file.startsWith("/song-library-references/setlist-idea/");
 
-  if (!baseUrl || /^(https?:|data:|blob:)/i.test(file)) {
+  if (!baseUrl || isBundledSetlistReference || /^(https?:|data:|blob:)/i.test(file)) {
     return file;
   }
 
